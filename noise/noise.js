ae86d0214da13cf5f3cc97dc798b24817b965523
Xorol <88636352+Xorol@users.noreply.github.com>
2021-11-18 18:49:59 -0800

    Romoved noise things

--- noise/noise.js
+++ noise/noise.js
@@ -1,19 +1,4 @@
-// noprotect
-var soundFile = document.createElement("audio");
 function setup() {
-  //Create the audio tag
-  soundFile.preload = "auto";
-
-  //Load the sound file (using a source element for expandability)
-  var src = document.createElement("source");
-  src.src =  "../tv-static-01.mp3";
-  soundFile.appendChild(src);
-  //Load the audio tag
-  //It auto plays as a fallback
-  soundFile.load();
-  soundFile.volume = 0.000000;
-  soundFile.play();
-
   createCanvas(window.innerWidth, window.innerHeight);
 }
 
@@ -24,14 +9,4 @@ function draw() {
     }
   }
   updatePixels();
-  playStatic(soundFile);
-}
-
-function playStatic(soundFile) {
-   //Set the current time for the audio file to the beginning
-   soundFile.currentTime = 0.01;
-   soundFile.volume = 100;
-
-   //Due to a bug in Firefox, the audio needs to be played after a delay
-   setTimeout(function(){soundFile.play();},1);
 }
