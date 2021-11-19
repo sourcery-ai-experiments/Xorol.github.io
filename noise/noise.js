77e85efc1208b754aa2a0c856d2fbd1c536783bd
Xorol <88636352+Xorol@users.noreply.github.com>
2021-11-18 18:40:42 -0800

    Update noise.js

--- noise/noise.js
+++ noise/noise.js
@@ -1,5 +1,19 @@
 // noprotect
 function setup() {
+  //Create the audio tag
+  var soundFile = document.createElement("audio");
+  soundFile.preload = "auto";
+
+  //Load the sound file (using a source element for expandability)
+  var src = document.createElement("source");
+  src.src = fileName + ".mp3";
+  soundFile.appendChild(src);
+  //Load the audio tag
+  //It auto plays as a fallback
+  soundFile.load();
+  soundFile.volume = 0.000000;
+  soundFile.play();
+
   createCanvas(window.innerWidth, window.innerHeight);
 }
 
@@ -10,4 +24,14 @@ function draw() {
     }
   }
   updatePixels();
+  playStatic();
+}
+
+function playStatic() {
+   //Set the current time for the audio file to the beginning
+   soundFile.currentTime = 0.01;
+   soundFile.volume = volume;
+
+   //Due to a bug in Firefox, the audio needs to be played after a delay
+   setTimeout(function(){soundFile.play();},1);
 }
