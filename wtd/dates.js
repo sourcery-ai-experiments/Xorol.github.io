0640cfd0c5836e49c8f62456bb9bbebcb0bcae97
Xorol <88636352+Xorol@users.noreply.github.com>
2021-11-25 13:27:32 -0800

    Create dates.js

---
+++ wtd/dates.js
@@ -0,0 +1,14 @@
+function dates() {
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let datetime1 = document.getElementById("date1").value.split("-").join(" ") + " " + document.getElementById("time1").value.split(":").join(" ");
+  let datetime2 = document.getElementById("date2").value.split("-").join(" ") + " " + document.getElementById("time2").value.split(":").join(" ");
+  datetime1 = datetime1.split(" ");
+  datetime2 = datetime2.split(" ");
+  const unix2 = new Date(months[parseInt(datetime2[1]) - 1] + " " + datetime2[2] + ", " + datetime2[0] + " " + [datetime2[3], datetime2[4], datetime2[5]].join(":"));
+  const unix1 = new Date(months[parseInt(datetime1[1]) - 1] + " " + datetime1[2] + ", " + datetime1[0] + " " + [datetime1[3], datetime1[4], datetime1[5]].join(":"));
+  if (isNaN(unix1 - unix2)) {
+    document.getElementById("output").innerHTML = "You missed a field or two!";
+    return;
+  }
+  document.getElementById("output").innerHTML = Math.abs(unix1 - unix2).toString().split("").reverse().join("").match(/.{1,3}/g).reverse().join(",");
+}
